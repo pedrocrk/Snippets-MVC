15ff435a049770ef32bf3ff8c2817cd37a4ce384
pedrocrk <36545349+pedrocrk@users.noreply.github.com>
2018-04-03 14:03:42 -0500

    Update misellaneus.js

--- Js/misellaneus.js
+++ Js/misellaneus.js
@@ -1,5 +1,9 @@
+//-- ===================================================================
+//-- Author:		Pedro Vargas Licona
+//-- Create date:   03/04/2018
+//-- Description:	Snippets Misellaneus Jquery 
+//-- ===================================================================
 
-
-//Add and remove rules of elements from jquery
+//Add and remove rules requireds of elements from jquery
 $("#Element").rules("remove", "required")
 $("#Element").rules("add", "required")
